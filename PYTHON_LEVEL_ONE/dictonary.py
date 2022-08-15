ecf622b8e68675839d8c81df9e57a4d12c7b62d1
Avnish Kumar <avnishgolu927@gmail.com>
2022-08-16 01:38:22 +0530

    Dictonary is Added

---
+++ PYTHON_LEVEL_ONE/dictonary.py
@@ -0,0 +1,12 @@
+
+dict={"A":"Pasta","B":"Sandwitch","C":"Momoes"}
+
+print(dict)
+
+dict['F']=["Value","Idea","Inovation"]
+
+print(dict);
+
+print("Value For the F 0-th element is : ", dict['F'][0])
+print("Value For the F 1-st element is : ", dict['F'][1])
+print("Value For the F 2-nd element is : ", dict['F'][2])
