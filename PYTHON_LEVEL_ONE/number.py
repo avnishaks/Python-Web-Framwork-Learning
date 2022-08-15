21490991c093743da50082219cf781a359f544e1
Avnish Kumar <avnishgolu927@gmail.com>
2022-08-16 00:19:51 +0530

    Number Part-1 added

--- PYTHON_LEVEL_ONE/number.py
+++ PYTHON_LEVEL_ONE/number.py
@@ -1,1 +1,19 @@
 print(1+2)
+
+a=10
+a=a*5
+print(a)
+print(2*a)
+
+my_tax=10000
+my_income=1.98
+
+total_cost=my_tax*my_income;
+
+print('Total cost ' , total_cost)
+
+print('Value',5)
+
+name="AKS"
+
+print('My name is',name)
