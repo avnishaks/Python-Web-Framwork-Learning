680e69463a86fed8e03b05fe3d2c672472d106ac
Avnish Kumar <avnishgolu927@gmail.com>
2022-08-20 20:36:33 +0530

    Name and Main File Concept in Python

---
+++ PYTHON_LEVEL_TWO/Name_Main_Two.py
@@ -0,0 +1,9 @@
+from Name_Main import fun
+print("Name_Main_Two.py File is Running")
+
+fun()
+
+if __name__ == '__main__':
+    print("Name_Main_Two.py is Running under this Sections")
+else:
+    print("Name_Main_Two.py is Runing under Package Sections")
